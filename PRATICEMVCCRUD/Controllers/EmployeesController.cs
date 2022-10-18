2e4d4ee06333d9b4fa80abc853886a1065ec7a0b
Abdoulaye Bah <dambubah@gmail.com>
2022-10-18 13:34:15 -0400

    Add project files.

---
+++ PRATICEMVCCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PRATICEMVCCRUD.Data;
+using PRATICEMVCCRUD.Models;
+using PRATICEMVCCRUD.Models.Domain;
+namespace PRATICEMVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        //The readonly field is to talk to our database.Var employee = new Employee()
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+        //this is the Index nethod that will the list of employee to the user.
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employee = await mvcDemoDbContext.Employees.ToListAsync();
+            return View(employee);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(AddEmployeeViewModelcs addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+
+            };
+
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
+
+
+        [HttpGet]
+       public async Task<IActionResult> View(Guid Id)
+        {
+           var employee = await mvcDemoDbContext.Employees.FirstOrDefaultAsync(x => x.Id == Id);
+
+            if (employee != null)
+            {
+                var viewModel = new UpdateViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    Department = employee.Department,
+                    DateOfBirth = employee.DateOfBirth
+                };
+                return await Task.Run(() => View("View", viewModel));
+            }
+            return RedirectToAction("Index");
+
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateViewModel model)
+        {
+            var employee = await mvcDemoDbContext.Employees.FindAsync(model.Id);
+
+            if (employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.DateOfBirth = model.DateOfBirth;
+                employee.Department = model.Department;
+
+                await mvcDemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateViewModel model)
+        {
+            var employee = await mvcDemoDbContext.Employees.FindAsync(model.Id);
+
+            if (employee != null)
+            {
+                mvcDemoDbContext.Employees.Remove(employee);
+                await mvcDemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
